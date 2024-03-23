ba838ea22a53afb5f2650786563cea1278e52490
Marcel22R <marieder22@gmail.com>
2024-03-23 20:33:02 +0100

    Added contact page html

--- kraftkammerPano/src/routes/contact/+page.svelte
+++ kraftkammerPano/src/routes/contact/+page.svelte
@@ -1,1 +1,18 @@
-div{Maintained
+
+<script>
+
+</script>
+
+<style>
+    .contact-details {
+        text-align: center;
+    }
+</style>
+
+<div class="contact-page">
+    <div class="contact-details">
+        <h1>Contact Me</h1>
+        <p>Feel free to reach out to us using the following contact details:</p>
+        <p>Email: marieder22@gmail.com</p>
+        </div>
+</div>
